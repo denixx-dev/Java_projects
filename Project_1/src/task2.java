9071b8431050167cb6f1701719c908359f5cc242
Denis <denissm2002@yandex.ru>
2021-02-12 22:59:49 +0300

    Project_1_edited_5

--- Project_1/src/task2.java
+++ Project_1/src/task2.java
@@ -2,9 +2,12 @@ import java.util.Scanner;
 
 public class task2 {
     public static void main(String[] args){
-
+        Scanner scn = new Scanner(System.in);
+        int a = scn.nextInt();
+        int b = scn.nextInt();
+        System.out.print(points(a,b));
     }
     public static int points(int a, int b){
-        
+        return (2*a + 3*b);
     }
 }
