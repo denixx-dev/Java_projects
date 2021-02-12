cec1959318f2a2f6e4b1fdf6aa269bce0f95a932
Denis <denissm2002@yandex.ru>
2021-02-13 00:30:14 +0300

    Project_1_edited_9

--- Project_1/src/task4.java
+++ Project_1/src/task4.java
@@ -1,5 +1,20 @@
 import java.util.Scanner;
 
 public class task4 {
-    
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        int a = scn.nextInt();
+        System.out.print(divisibleByFive(a));
+
+    }
+    public static boolean divisibleByFive(int a){
+        boolean flag;
+        if (a % 5 > 0){
+            flag = false;
+        }
+        else{
+            flag = true;
+        }
+        return (flag);
+    }
 }
