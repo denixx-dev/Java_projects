811e521cb7d1b696024b38184568e9a9b63fdd2d
Denis <denissm2002@yandex.ru>
2021-02-13 01:11:37 +0300

    Project_1_edited_10

---
+++ Project_1/src/task5.java
@@ -0,0 +1,29 @@
+import java.lang.String;
+import java.util.Scanner;
+
+public class task5 {
+    public static void main(String[] args){
+        Scanner a = new Scanner(System.in);
+        Scanner b = new Scanner(System.in);
+        int x = a.nextInt();
+        int y = b.nextInt();
+        System.out.print(and(x,y));
+    }
+    public static int and(int a, int b){
+        int res;
+
+        /*if ((a == 1) & (b == 1)){
+            res = true;
+        }
+        else if ((a==0)&(b==0)){
+            res = false;
+        }
+        else if ((a==0)&(b==1)){
+            res = false;
+        }
+        else if ((a ==1)&(b ==0)){
+            res = false;
+        }*/
+        return (res);
+    }
+}
