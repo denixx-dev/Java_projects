392d093dc77fcfa19f7b839daf2f9541d82bbadb
Denis <denissm2002@yandex.ru>
2021-02-13 00:03:09 +0300

    Project_1_edited_7

--- Project_1/src/task3.java
+++ Project_1/src/task3.java
@@ -1,2 +1,15 @@
+import java.util.Scanner;
+
 public class task3 {
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        int a = scn.nextInt();
+        int b = scn.nextInt();
+        int c = scn.nextInt();
+        System.out.print(footballPoints(a,b,c));
+
+    }
+    public static int footballPoints(int a, int b, int c){
+        return (3*a + b + 0*c);
+    }
 }
