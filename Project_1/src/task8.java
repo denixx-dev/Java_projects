e915ca6109415a3ca3daed28e5f8a2caccf7dacf
Denis <denissm2002@yandex.ru>
2021-02-13 23:20:28 +0300

    Project_1_edited_14

---
+++ Project_1/src/task8.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class task8 {
+    public static void main (String[] args){
+        Scanner scn = new Scanner(System.in);
+        float prob = scn.nextFloat();
+        int prize = scn.nextInt();
+        int pay = scn.nextInt();
+        System.out.println(profitableGamble(prob, prize, pay));
+    }
+    public static boolean profitableGamble(float a, int b, int c){
+        boolean res = false;
+        if (a*b > c){
+            res = true;
+        }
+        return (res);
+    }
+}
