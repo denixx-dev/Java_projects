25aa9cac777cd865f06296f20fc7bbc548051d0e
Denis <denissm2002@yandex.ru>
2021-02-14 00:17:26 +0300

    Project_1_edited_16

---
+++ Project_1/src/task10.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class task10 {
+    public static void main (String[] args){
+        Scanner scn = new Scanner(System.in);
+        int x = scn.nextInt();
+        int y = scn.nextInt();
+        System.out.println(mod(x,y));
+    }
+    public static int mod (int a, int b){
+        int res =0;
+        while (a >= b){
+            a= a -b;
+            res = a;
+        }
+        return (res);
+    }
+}
