810b99711624dc30209f531c9f4f7c9edc0e5565
Denis <denissm2002@yandex.ru>
2021-02-13 21:16:34 +0300

    Project_1_edited_11

--- Project_1/src/task5.java
+++ Project_1/src/task5.java
@@ -4,26 +4,29 @@ import java.util.Scanner;
 public class task5 {
     public static void main(String[] args){
         Scanner a = new Scanner(System.in);
-        Scanner b = new Scanner(System.in);
-        int x = a.nextInt();
-        int y = b.nextInt();
+        //Scanner b = new Scanner(System.in);
+        boolean x = false;
+        try {
+            x = a.nextBoolean();
+        } catch (Exception e) {
+            System.out.println("Неверно введены даные");
+        }
+        boolean y = false;
+        try {
+            y = a.nextBoolean();
+        } catch (Exception e) {
+            System.out.println("Неверно введены данные");
+        }
         System.out.print(and(x,y));
     }
-    public static int and(int a, int b){
-        int res;
-
-        /*if ((a == 1) & (b == 1)){
+    public static boolean and(boolean a, boolean b){
+        boolean res = true;
+        if ((a == true) & (b == true)){
             res = true;
         }
-        else if ((a==0)&(b==0)){
+        else {
             res = false;
         }
-        else if ((a==0)&(b==1)){
-            res = false;
-        }
-        else if ((a ==1)&(b ==0)){
-            res = false;
-        }*/
         return (res);
     }
 }
