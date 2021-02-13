974dc649d2a398347d1489889bbb480da751f053
Denis <denissm2002@yandex.ru>
2021-02-14 00:07:23 +0300

    Project_1_edited_15

---
+++ Project_1/src/task9.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class task9 {
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        int x = scn.nextInt();
+        int y = scn.nextInt();
+        System.out.print(frames(x,y));
+    }
+    public static int frames(int a , int b ){
+        return (a*b*60);
+    }
+}
