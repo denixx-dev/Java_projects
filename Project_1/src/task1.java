9b5be7b3d7b42916e59e745a0c9e0c42ac57a486
Denis <denissm2002@yandex.ru>
2021-02-14 10:43:10 +0300

    Project_1_edited_17

--- Project_1/src/task1.java
+++ Project_1/src/task1.java
@@ -3,8 +3,10 @@ import java.lang.String;
 
 public class task1 {
     public static void main(String[] args){
+        System.out.print("Введите кол-во минут: ");
         Scanner scn = new Scanner(System.in);
         float f = scn.nextFloat();
+        System.out.print("Кол-во секунд: ");
         System.out.print(minsecconv(f));
         //System.out.print (scn.nextLine ());
     }
