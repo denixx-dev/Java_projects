c15db083f83db0703b0edb2d842052b28b9e1064
Denis <denissm2002@yandex.ru>
2021-02-14 11:10:08 +0300

    Project_1_edited_20

--- Project_1/src/task4.java
+++ Project_1/src/task4.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class task4 {
     public static void main(String[] args){
         Scanner scn = new Scanner(System.in);
+        System.out.println("Введите число для проверки его на целое деление на 5: ");
         int a = scn.nextInt();
         System.out.print(divisibleByFive(a));
 
