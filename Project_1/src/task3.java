98748a3fa378b2c1c0f86e1045a46be377dca2a7
Denis <denissm2002@yandex.ru>
2021-02-14 11:05:50 +0300

    Project_1_edited_19

--- Project_1/src/task3.java
+++ Project_1/src/task3.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class task3 {
     public static void main(String[] args){
         Scanner scn = new Scanner(System.in);
+        System.out.println("Введите кол-во побед, проигрышей и ничьих: ");
         int a = scn.nextInt();
         int b = scn.nextInt();
         int c = scn.nextInt();
