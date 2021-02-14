b5b563cf12929ccdd6b2bf4727083941111fd0df
Denis <denissm2002@yandex.ru>
2021-02-14 11:02:20 +0300

    Project_1_edited_18

--- Project_1/src/task2.java
+++ Project_1/src/task2.java
@@ -3,7 +3,10 @@ import java.util.Scanner;
 public class task2 {
     public static void main(String[] args){
         Scanner scn = new Scanner(System.in);
+        System.out.print("Введите кол-во 2-х очковых бросков: ");
         int a = scn.nextInt();
+        System.out.println(' ');
+        System.out.print("Введите кол-во 3-х очковых бросков: ");
         int b = scn.nextInt();
         System.out.print(points(a,b));
     }
