02b55fb839b1b3bd5db403626dbc9ca2b5583b44
Denis <denissm2002@yandex.ru>
2021-02-14 11:16:17 +0300

    Project_1_edited_21

--- Project_1/src/task5.java
+++ Project_1/src/task5.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class task5 {
     public static void main(String[] args){
         Scanner a = new Scanner(System.in);
-        //Scanner b = new Scanner(System.in);
+        System.out.println("Введите два булевых значение: ");
         boolean x = false;
         try {
             x = a.nextBoolean();
