31d26279b75efc4908f7e13be94149ad3d054a06
Denis <denissm2002@yandex.ru>
2021-02-21 21:53:33 +0300

    JavaLab1_edited_1

--- JavaLab1/src/Primes.java
+++ JavaLab1/src/Primes.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Primes {
     public static void main (String[] args){
-        
+        for (int j =2; j <= 100; j++){
+          if (isPrime(j)==false){
+              System.out.print(j + " ");
+          }
+        }
+    }
+    public static boolean isPrime(int n){
+        boolean flag=false;
+        int i;
+        for (i=2; i < n; i++){
+            if (n%i==0){
+                flag =true;
+                break;
+            }
+        }
+        return(flag);
     }
 }
