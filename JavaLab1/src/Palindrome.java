f89b0061d9eefc720cf1ff520335eb14c8d84ffa
Denis <denissm2002@yandex.ru>
2021-02-26 13:07:30 +0300

    JavaLab1_edited_3

--- JavaLab1/src/Palindrome.java
+++ JavaLab1/src/Palindrome.java
@@ -1,30 +1,17 @@
+import java.util.Locale;
 import java.util.Scanner;
 import java.lang.String;
 
 public class Palindrome {
     public static void main(String[] args){
-       /*for (int i =0; i< args.length;i++){
-           String s = args[i];
-       }*/
-       int space =0;
        Scanner scn = new Scanner(System.in);
        String text = scn.nextLine();
-       int len = text.length();
-       for (int i=0; i<text.length();i++){
-           if (text.charAt(i)== ' '){
-               space++;
-           }
-       }
-       space++;
-       System.out.print(space);
-       System.out.print(reverseString(text));
+        String[] sp = text.split(" ");
+        for (int i=0; i < sp.length; i++){
+            System.out.print(reverseString(sp[i]) + " ");
+        }
     }
-    public static boolean reverseString(String x){
-        /*String str = "";
-        int len = x.length();
-        for (int j=len; j > 0; j--){
-            str += x[j];
-        }*/
+    private static boolean reverseString(String x){
         return(x.replaceAll("\\W","")
                 .equalsIgnoreCase(new StringBuilder(x.replaceAll("\\W",""))
                         .reverse().toString()));
