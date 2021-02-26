08b2e4c73224649c80a7818bb9cddce2b0ca37dd
Denis <denissm2002@yandex.ru>
2021-02-26 13:10:42 +0300

    JavaLab1_edited_4

--- JavaLab1/src/Palindrome.java
+++ JavaLab1/src/Palindrome.java
@@ -8,10 +8,10 @@ public class Palindrome {
        String text = scn.nextLine();
         String[] sp = text.split(" ");
         for (int i=0; i < sp.length; i++){
-            System.out.print(reverseString(sp[i]) + " ");
+            System.out.print(isPalindrome(sp[i]) + " ");
         }
     }
-    private static boolean reverseString(String x){
+    private static boolean isPalindrome(String x){
         return(x.replaceAll("\\W","")
                 .equalsIgnoreCase(new StringBuilder(x.replaceAll("\\W",""))
                         .reverse().toString()));
