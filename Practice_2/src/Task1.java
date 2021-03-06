a925a86dd94dc5f192b7e7f45979b3335ddf3abe
Denis <denissm2002@yandex.ru>
2021-03-07 01:19:08 +0300

    Practice_2_added

---
+++ Practice_2/src/Task1.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main (String[] args){
+    Scanner scn = new Scanner(System.in);
+    int a = scn.nextInt();
+    int b = scn.nextInt();
+    /*for (int i=0;i<b;i++){
+        for (int j =0;j<2;j++){
+            System.out.print(Arr[i][j]);
+        }
+        System.out.println("");
+    }*/
+    System.out.print(oppositeHouse(a,b));
+    }
+
+    private static int oppositeHouse(int x, int b) {
+        int numCol1 =1;
+        int numCol2 = 2*b;
+        int res=0;
+        int[][] Arr = new int[b+1][3];
+        for (int i=1;i<=b;i++){
+            Arr[i][1]=numCol1;
+            numCol1 += 2;
+        }
+        for(int i =1;i<=b;i++){
+            Arr[i][2]= numCol2;
+            numCol2 -= 2;
+        }
+        for (int i=1; i<=b;i++){
+            for (int j =1; j<3;j++){
+                if ((Arr[i][j] == x)){
+                    if((Arr[i][j] % 2) ==0){
+                        res = Arr[i][j-1];
+                    }
+                    if ((Arr[i][j] % 2) !=0) {
+                        res = Arr[i][j+1];
+                    }
+                }
+            }
+        }
+        return (res);
+    }
+}
---
+++ Practice_2/src/Task2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main (String[] args){
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Введите строку: ");
+        String str = scn.nextLine();
+        System.out.print(nameShuffle(str));
+    }
+    private static String nameShuffle(String x){
+        String[] dividedName = x.split(" ");
+        String shuffledName = dividedName[1] + " " + dividedName[0];
+        return (shuffledName);
+    }
+}
