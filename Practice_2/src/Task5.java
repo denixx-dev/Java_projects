da6f8080fcf34550e15b23ff19c98d960ea2e3ba
Denis <denissm2002@yandex.ru>
2021-03-09 19:35:30 +0300

    Task5_added

---
+++ Practice_2/src/Task5.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Task5 {
+    public static void main (String[] args){
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        System.out.print(equal(a,b,c));
+    }
+    public static int equal(int a, int b, int c){
+        int kol=0;
+        if (a==b){
+            kol=2;
+        }
+        if(b==c){
+            kol=2;
+        }
+        if(a==c){
+            kol=2;
+        }
+        if((a==b)&(a==c)&(b==c)){
+            kol++;
+        }
+        return(kol);
+    }
+}
