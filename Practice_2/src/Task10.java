0de6fe301ebc6b741c77ec9d39c5bc6efd5b6615
Denis <denissm2002@yandex.ru>
2021-03-11 14:29:50 +0300

    Practice_2 edited

--- Practice_2/src/Task10.java
+++ Practice_2/src/Task10.java
@@ -1,8 +1,18 @@
 public class Task10 {
     public static void main(String[] args){
-
+        System.out.print(sameAscii("EdAbit", "EDABIT"));
     }
     public static boolean sameAscii(String a, String b){
-        
+        char[] aParsed = a.toCharArray();
+        char[] bParsed = b.toCharArray();
+        int aAscii=0;
+        int bAscii=0;
+        for (int i=0; i< aParsed.length;i++){
+            aAscii += (int)aParsed[i];
+        }
+        for(int i=0; i< bParsed.length;i++){
+            bAscii += (int)bParsed[i];
+        }
+        return (aAscii==bAscii);
     }
 }
