0e6b87825b19b5aeb4adc332acb55b32dbde3cc1
denixx-dev <denissm2002@yandex.ru>
2021-03-20 21:59:54 +0300

    Added JavaLab3

---
+++ JavaLab3/src/Location.java
@@ -0,0 +1,37 @@
+import java.util.Objects;
+
+public class Location {
+    /** X coordinate of this location. **/
+    public int xCoord;
+
+
+    /** Y coordinate of this location. **/
+    public int yCoord;
+
+
+    /** Creates a new location with the specified integer coordinates. **/
+    public Location(int x, int y)
+    {
+        xCoord = x;
+        yCoord = y;
+    }
+
+    /** Creates a new location with coordinates (0, 0). **/
+    public Location()
+    {
+        this(0, 0);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Location location = (Location) obj;
+        return xCoord == location.xCoord && yCoord == location.yCoord;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(xCoord, yCoord);
+    }
+}
