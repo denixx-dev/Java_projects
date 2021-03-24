2ab10a9c2f89b3dab8e447300b769d29763b6323
denixx-dev <denissm2002@yandex.ru>
2021-03-24 15:56:42 +0300

    Edited Practice_3 1

--- Practice_3/src/Task1.java
+++ Practice_3/src/Task1.java
@@ -2,19 +2,36 @@ import java.util.ArrayList;
 
 public class Task1 {
     public static void main (String[] args){
-        String[] townAndPopulation = new String[]{"Nepal", "2186853"};
+        String[] townAndPopulation = new String[]{"Nepal", "572"};
         System.out.print(millionsRounding(townAndPopulation));
     }
-    public static double millionsRounding(String[] townsAndPopulation){
+    public static String millionsRounding(String[] townsAndPopulation){
+        //String town = townsAndPopulation[0];
+        String pop = townsAndPopulation[1];
+        /**
+         * Представление строки в виде числа
+         */
         int population = Integer.parseInt(townsAndPopulation[1]);
-        int length = townsAndPopulation[1].length();
-        double diff = Math.pow(10,length)-population;
-        if ((diff <= 500000)&(population < Math.pow(10,length))) {
-            return Math.pow(10,length);
+        //int length = townsAndPopulation[1].length();
+        int div = (int)(population / Math.pow(10,6));
+        int res=0;
+        /**
+         * Проверка на то, является ли число меньшим, чем миллион
+         */
+        if (div == 0){
+            double diff = Math.pow(10,6)*(div+1)-population;
+            if (diff <= 500000){
+                res = (int)Math.pow(10,6)*(div+1);
+            }
         }
         else{
-            return 0;
+            double diff = population-Math.pow(10,6)*div;
+            if(diff <= 500000){
+                res = (int)Math.pow(10,6)*div;
+            }
         }
-        //return 0;
+        pop = Integer.toString(res);
+        return townsAndPopulation[0]+' '+pop;
+
     }
 }
