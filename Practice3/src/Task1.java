27c18a259e0790d7ee66b831f3e4769f5050f21f
Denis <denissm2002@yandex.ru>
2021-03-27 18:41:56 +0300

    JavaLab3 edited 3

---
+++ Practice3/src/Task1.java
@@ -0,0 +1,10 @@
+public class Task1 {
+    public static void main(String[] args) {
+        String[] arr = new String[]{"fdfdg", "12"};
+    }
+    public static String[] millionsRounding(String[] x){
+
+        return (x);
+    }
+
+}
