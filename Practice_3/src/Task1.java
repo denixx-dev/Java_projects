bcdf56c64110dc022954e6d48887d8023bb52375
denixx-dev <denissm2002@yandex.ru>
2021-03-27 16:29:04 +0300

    Edited Practice_3 3

--- Practice_3/src/Task1.java
+++ Practice_3/src/Task1.java
@@ -4,24 +4,24 @@ import java.util.HashMap;
 public class Task1 {
     public static void main (String[] args){
         HashMap<String, Integer> townAndPopulation = new HashMap<>();
-        townAndPopulation.put("Nice", 942208);
-        townAndPopulation.put("Abu Dhabi", 1482816);
+        townAndPopulation.put("Nice", 11200000);
+        townAndPopulation.put("Abu Dhabi", 499000);
         townAndPopulation.put("Naples", 2186853);
+        townAndPopulation.put("Moscow", 15500000);
 
 
         System.out.println(millionsRounding(townAndPopulation));
     }
     public static HashMap<String, Integer> millionsRounding(HashMap<String, Integer> a){
-        int pop1 = a.get("Nice");
-        int pop2 = a.get("Abu Dhabi");
-        int pop3 = a.get("Naples");
-
-        int res1 = (int)(Math.round((pop1 / 1E6))*1E6);
-        a.put("Nice", res1);
-        int res2 = (int)(Math.round((pop2 / 1E6))*1E6);
-        a.put("Abu Dhabi", res2);
-        int res3 = (int)(Math.round((pop3 / 1E6))*1E6);
-        a.put("Naples", res3);
+        ArrayList<Integer> values = new ArrayList<>(a.values());
+        int eachPop=0;
+        int i=0;
+        ArrayList<String> keys = new ArrayList<>(a.keySet());
+        for (Integer value : values) {
+            eachPop = (int)(Math.round(value/1E6)*1E6);
+            a.replace(keys.get(i), eachPop);
+            i++;
+        }
 
         return a;
 
