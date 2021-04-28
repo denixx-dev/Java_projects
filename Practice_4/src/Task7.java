741c39ec21dbdb2fb70d57c00e95a489fb56d89a
denixx-dev <denissm2002@yandex.ru>
2021-04-28 23:23:38 +0300

    Edited Practice_4 4

--- Practice_4/src/Task7.java
+++ Practice_4/src/Task7.java
@@ -1,9 +1,50 @@
 public class Task7 {
     public static void main (String[] args) {
-
+        undefined(8784);
     }
-    /*public static int undefined (int num) {
+    public static void undefined (int num) {
         String strNum = Integer.toString(num);
 
-    }*/
+        char[] charStr = strNum.toCharArray();
+
+        if (charStr.length  == 3) {
+            char k = charStr[0];
+            charStr[0] = charStr[2];
+            charStr[2] = k;
+        }
+        if (charStr.length == 4) {
+            char k = charStr[1];
+            charStr[1] = charStr[3];
+            charStr[3] =k;
+        }
+        if (charStr.length ==2) {
+            char k = charStr[0];
+            charStr[0] = charStr[1];
+            charStr[1] =k;
+        }
+
+        /*if (charStr.length % 2 != 0) {
+            for (int i =0; i< charStr.length;i+=2) {
+                if (i +2 != charStr.length-1) {
+                    char replaceChr = charStr[i];
+                    charStr[i] = charStr[i+2];
+                    charStr[i+2] = replaceChr;
+                }
+               *//* else{
+                    char replaceChr = charStr[i];
+                    charStr[i] = charStr[i-2];
+                    charStr[i-2] = replaceChr;
+                }*//*
+            }
+        }*/
+
+
+        int newInt = Integer.parseInt(new String(charStr));
+
+        if (num - newInt < 0) {
+            System.out.println("0");
+        }
+        else
+        System.out.println(num - newInt);
+    }
 }
