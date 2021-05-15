e42deb42b33637627e7e8bb79cd725fe38478b38
denixx-dev <denissm2002@yandex.ru>
2021-05-15 20:00:57 +0300

    Edited Practice 4 6

---
+++ Practice_4/src/Task8.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Task8 {
+    public static void main (String[] args) {
+        System.out.print(commonLastVowel("OOI UUI EEI AAAAI"));
+    }
+    public static String commonLastVowel (String str) {
+        str = str.toLowerCase().replaceAll("[,.!?:;-]+", "");
+        List<String> s = Arrays.asList("euioay".split(""));
+        String[] w = str.split(" ");
+        HashMap<Integer, String> map = new HashMap<>();
+        int k = 0;
+        for (String t : w) {
+            String v = t.substring(t.length() - 1);
+            if (s.contains(v)) {
+                map.put(k, v);
+                k++;
+            }
+        }
+        return map.get(k - 1);
+    }
+}
